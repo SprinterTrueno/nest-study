8f2357c84d622109038bcbe5071d01764f363925
oe_malin <malin_hz@hnzycfc.com>
2024-04-25 20:29:33 +0800

    Handling request body payload

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, Post, Body } from "@nestjs/common";
 
 @Controller("coffees")
 export class CoffeesController {
@@ -10,5 +10,13 @@ export class CoffeesController {
   @Get(":id")
   findOne(@Param("id") id: string) {
     return `Coffee #${id}`;
+  }
+
+  @Post()
+  create(@Body() body: { name: string }) {
+    // create(@Body("name") name: string) {
+    // return "Coffee created";
+    return body;
+    // return name;
   }
 }
