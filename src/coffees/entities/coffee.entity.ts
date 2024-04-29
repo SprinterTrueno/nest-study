7d586627063059b5e0b8ee068a83a12943748ac1
oe_malin <malin_hz@hnzycfc.com>
2024-04-29 17:23:44 +0800

    Creating a typeorm entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  // 将 flavors 字段设置为 JSON 类型，并允许为空。
+  @Column("json", { nullable: true })
   flavors: string[];
 }
