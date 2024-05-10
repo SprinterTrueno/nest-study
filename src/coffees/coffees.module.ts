c77962f8c5637257443798963371f7ac15966fec
oe_malin <malin_hz@hnzycfc.com>
2024-05-10 15:48:41 +0800

    Leverage async providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Injectable, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Event } from "@/events/entities/event.entity";
 import { COFFEE_BRANDS } from "./coffees.constants";
@@ -7,36 +7,16 @@ import { CoffeesService } from "./coffees.service";
 import { Coffee } from "./entities/coffee.entity";
 import { Flavor } from "./entities/flavor.entity";
 
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
-
-@Injectable()
-export class CoffeeBrandsFactory {
-  createCoffeeBrands() {
-    return ["buddy brew", "nescafe"];
-  }
-}
-
 @Module({
   imports: [TypeOrmModule.forFeature([Event, Coffee, Flavor])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
     {
-      provide: ConfigService,
-      useClass:
-        process.env.NODE_ENV === "development"
-          ? DevelopmentConfigService
-          : ProductionConfigService
-    },
-    CoffeeBrandsFactory,
-    {
       provide: COFFEE_BRANDS,
-      useFactory: (coffeeBrandsFactory: CoffeeBrandsFactory) => {
-        return coffeeBrandsFactory.createCoffeeBrands();
-      },
-      inject: [CoffeeBrandsFactory]
+      useFactory: async () => {
+        return await Promise.resolve(["buddy brew", "nescafe"]);
+      }
     }
   ],
   exports: [CoffeesService]
